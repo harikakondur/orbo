63a726a2f0be8fb889478da07e890d7ab85ec7e2
Harika <harikakondur3@gmail.com>
2024-10-22 10:29:49 -0600

    updated pickaxe bot

--- pages/content-ui/src/App.tsx
+++ pages/content-ui/src/App.tsx
@@ -16,18 +16,12 @@ export default function App() {
 
   return (
     <>
-      <div className="flex items-center justify-between gap-2 rounded bg-blue-100 px-2 py-1">
-        <div className="flex gap-1 text-blue-500">
-          Edit <strong className="text-blue-700">pages/content-ui/src/app.tsx</strong> and save to reload.
-        </div>
-        <Button theme={theme} onClick={exampleThemeStorage.toggle}>
-          Toggle Theme
-        </Button>
-      </div>
       {isCartPage() && (
-        <div className="w-screen h-screen fixed bg-red-500/10 left-0 top-0 z-[1000] grid place-items-center">
-          <button className="px-4 py-2 font-bold bg-white border" onClick={handleOpenSidePanel}>
-            Open Sidepanel
+        <div className="w-screen h-screen fixed bg-green-500/10 left-0 top-0 z-[1000] grid place-items-center">
+          <button
+            className="px-8 py-2 font-bold bg-white border text-xl rounded-md bg-green-400"
+            onClick={handleOpenSidePanel}>
+            Wait! Lets think about this.
           </button>
         </div>
       )}
--- pages/side-panel/src/SidePanel.tsx
+++ pages/side-panel/src/SidePanel.tsx
@@ -8,8 +8,8 @@ const SidePanel = () => {
   const isLight = theme === 'light';
 
   return (
-    <div className={`App ${isLight ? 'bg-slate-50' : 'bg-gray-800'}`}>
-      <header className={`App-header ${isLight ? 'text-gray-900' : 'text-gray-100'}`}>
+    <div className="bg-slate-50">
+      <header className="text-gray-900">
         {/* Button to open the Typeform */}
         {/* <button
           onClick={openTypeform}
@@ -21,17 +21,30 @@ const SidePanel = () => {
         <iframe
           id="embed-preview-iframe"
           loading="eager"
-          src="https://embed.pickaxeproject.com/axe?id=Financial_Advisor_Bot_K38T3&mode=embed_gold&host=beta&theme=light&opacity=100&font_header=Real+Head+Pro&size_header=30&font_body=Real+Head+Pro&size_body=16&font_labels=Real+Head+Pro&size_labels=14&font_button=Real+Head+Pro&size_button=16&c_fb=FFFFFF&c_ff=FFFFFF&c_fbd=888888&c_rb=FFFFFF&c_bb=228DD7&c_bt=FFFFFF&c_t=000000&s_ffo=100&s_rbo=100&s_bbo=100&s_f=minimalist&s_b=filled&s_t=1&s_to=1&s_r=2"
+          src="https://embed.pickaxeproject.com/axe?id=Financial_Advisor_Bot_K38T3&mode=embed_gold&host=beta&theme=custom&opacity=100&font_header=Real+Head+Pro&size_header=30&font_body=Real+Head+Pro&size_body=16&font_labels=Real+Head+Pro&size_labels=14&font_button=Real+Head+Pro&size_button=16&c_fb=FFFFFF&c_ff=FFFFFF&c_fbd=FFFFFF&c_rb=FFFFFF&c_bb=649E6A&c_bt=FFFFFF&c_t=000000&s_ffo=100&s_rbo=100&s_bbo=100&s_f=minimalist&s_b=filled&s_t=1&s_to=1&s_r=2"
           width="100%"
           height="500px"
           className="transition hover:translate-y-[-2px] hover:shadow-[0_6px_20px_0px_rgba(0,0,0,0.15)]"
           style={{
-            border: '1px solid rgba(0, 0, 0, 1)',
             transition: '.3s',
             borderRadius: '4px',
             maxWidth: '700px',
           }}
           frameBorder="0"></iframe>
+
+        {/* <iframe
+          id="embed-preview-iframe"
+          loading="eager"
+          src="https://embed.pickaxeproject.com/axe?id=Financial_Advisor_Bot_K38T3&mode=embed_gold&host=beta&theme=light&opacity=100&font_header=Real+Head+Pro&size_header=30&font_body=Real+Head+Pro&size_body=16&font_labels=Real+Head+Pro&size_labels=14&font_button=Real+Head+Pro&size_button=16&c_fb=FFFFFF&c_ff=FFFFFF&c_fbd=888888&c_rb=FFFFFF&c_bb=228DD7&c_bt=FFFFFF&c_t=000000&s_ffo=100&s_rbo=100&s_bbo=100&s_f=minimalist&s_b=filled&s_t=1&s_to=1&s_r=2"
+          width="100%"
+          height="500px"
+          className="transition hover:translate-y-[-2px] hover:shadow-[0_6px_20px_0px_rgba(0,0,0,0.15)]"
+          style={{
+            transition: '.3s',
+            borderRadius: '4px',
+            maxWidth: '700px',
+          }}
+          frameBorder="0"></iframe> */}
       </header>
     </div>
   );
